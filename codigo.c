0f00daea5cccbf614b13e372f8fb30a924ed6a2e
Ian <Ian612040>
2016-08-14 18:41:23 -0300

    codigo

---
+++ codigo.c
@@ -0,0 +1,15 @@
+/*
+Universidade Federal de Sergipe
+Trabalho 02 - Arquitetura de Computadores
+Aluno: Ian Vitor Barreto da Silva
+Matricula: 201320007439
+*/
+
+#include <stdio.h>
+ 
+void  main( )
+{
+	stc	;  // Cria o carry com tamanho 1
+	mov ah,10h	; //Copia o número binário 10h para o registrador
+	rcr ah,1	; // Rotaciona o registrador 1 vez para a direita
+}
